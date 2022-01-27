8d6ea9d0b7bec51d38ef28fb21cba7e6f4d30646
Rhenn Handel <rwhandel@gmail.com>
2022-01-26 19:25:00 -0500

    First commit, proof of concept. Can create simple alternatives based on wordnet.

---
+++ podlu.py
@@ -0,0 +1,81 @@
+import os
+import itertools
+from nltk.corpus import wordnet
+
+#discover and populate 2d array with all possible synonyms
+#run through first letters and see if any create real words
+#attempt to suggest alternatives based on vowel structure?
+
+#NEXT: insert words not found within the wordnet into the mixedSyns list anyways
+
+#grab phrases for acronym
+fullString = input("Enter your phrases, delineated by spaces: ")
+wordArray = fullString.split()
+wnLemmas = set(wordnet.all_lemma_names())
+synList = []
+failList = []
+#for each word given, populate a list of homonyms and definitions, removing direct synonyms
+for i, c in enumerate(wordArray):
+    if not(c in wnLemmas):
+        print("\"" + c + "\" not found in wordnet. No synonyms will be discovered.")
+        failList.append(i)
+        synList.append(c)
+        continue
+    tempSyns = wordnet.synsets(c)
+    homonym = c
+    tempSyns[:] = [h for h in tempSyns if h.name().split(".")[0] == homonym]
+    if not tempSyns:
+        print("Unfortunately, your word \"" + c + "\" exists but is not given a specified definition in Wordnet for whatever reason. I know, what the fuck.")
+        failList.append(i)
+        synList.append(c)
+        continue
+    synList.append(tempSyns)
+
+#query the user for their preferred definition for each word
+#populate 2d array (trueSyn) of synonyms of proper definition
+trueSyn = []
+for index, r in enumerate(synList):
+    #print(r)
+    if failList:
+        if index == failList[0]:
+            print(wordArray[index])
+            trueSyn.append([wordArray[index]])
+            failList.pop(0)
+            continue
+    print("Enter the index number of the definition you prefer for word " + str(index+1) + ", \"" + r[0].name().split(".")[0] + "\":")
+    for i, c in enumerate(r):
+        print("(" + str(i) + ") " + c.name().split(".")[1] + ". " + c.definition())
+    word = int(input("Index: "))
+    lemmas = r[word].lemma_names()
+    similars = [h.name().split(".")[0] for h in r[word].similar_tos()]
+    for h in similars:
+        if not(h in lemmas):
+            lemmas.append(h)
+    trueSyn.append(lemmas)
+    #print(r[word].lemma_names())
+    #print(r[word].similar_tos())
+    #trueSyn.append([h.name().split(".")[0] for h in r[word].similar_tos()])
+
+#run through trueSyn to discover possible acronym combinations
+print(trueSyn)
+acroLength = len(trueSyn)
+acroList = []
+if acroLength <= 1:
+    print("Not enough phrases for acronym. Program will close.")
+    quit()
+mixedSyns = list(itertools.product(*trueSyn))
+#print(mixedSyns)
+for r in mixedSyns:
+    testCase = ""
+    for c in r:
+        testCase += c[0]
+    if not(testCase in wnLemmas):
+        print(testCase + " not found in wordnet. Rejected!")
+    else:
+        print(testCase + " is a word. Approved!")
+        acroList.append(r)
+    
+if not acroList:
+    print("Whoops, nothing nice found. Bye.")
+else:
+    print(acroList)
